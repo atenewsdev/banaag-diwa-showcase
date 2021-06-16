0eb2035fa39a94d6b6b1adccf3eb4315a83cf1dc
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-06-16 22:20:07 +0800

    add editors

--- src/data/contributors/editors.js
+++ src/data/contributors/editors.js
@@ -7,4 +7,12 @@ module.exports = [
     name: 'Sofia Roena Guan',
     bionote: `Sofia Roena Guan is a graduating Accountancy student at the Ateneo de Davao University. She has been serving Atenews since 2018 and is currently the Associate and Managing Editor. She doesn't write much because she likes working with numbers more.`
   },
+  {
+    name: `Julien Jame E. Apale`,
+    bionote: `Julien Jame E. Apale was a former Atenews art editor and editor-in-chief. She is currently a freelance writer for students in undergraduate and postgraduate programs.`
+  },
+  {
+    name: `Maria Cyra Jane Dealca`,
+    bionote: `Maria Cyra Jane Dealca was a former Art Editor and Cartoonist. As an aspiring Early Childhood Teacher, she hopes to engage with a five-year-old activist one day — possibly even younger!`
+  }
 ]
