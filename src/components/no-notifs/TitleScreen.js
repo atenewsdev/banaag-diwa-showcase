78a9e4b3cb324499d512b46d173d7506b9cb23d2
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-06-20 15:57:13 +0800

    add no notifs volume

--- src/components/no-notifs/TitleScreen.js
+++ src/components/no-notifs/TitleScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Center, Image, Button, SimpleGrid, Box } from '@chakra-ui/react';
+import { Center, Image, Button, SimpleGrid, Box, Text, VStack } from '@chakra-ui/react';
 
 import { CDN_URL } from '../../utils/constants';
 
@@ -9,9 +9,12 @@ const TitleScreen = ({ onStart }) => {
     <Box bgColor="#030303" bg="#030303">
       <Center w="100vw" h="100vh">
         <SimpleGrid columns={1} spacing={10}>
-          <Image src={`${CDN_URL}/2021/06/no-notifs.png`} alt="No Notifs" w="50vw" />
           <Center>
-            <Image src={`${CDN_URL}/2021/06/low-batt_edited.gif`} alt="Battery" w="15vw" />
+            <VStack>
+              <Image src={`${CDN_URL}/2021/06/no-notifs.png`} alt="No Notifs" w="50vw" />
+              <Image src={`${CDN_URL}/2021/06/low-batt_edited.gif`} alt="Battery" w="15vw" />
+              <Text fontStyle="italic" color="white">This story includes background music and sound effects. Turn up your volume for maximum experience.</Text>
+            </VStack>
           </Center>
           <Button colorScheme="whiteAlpha" variant="ghost" color="white" onClick={onStart}>
             Start Reading &gt;&gt;
